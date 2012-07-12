72a95de3fd61f433092106d2b9456ecb00d51d71
DJ Holt <dj@holt.dj>
2012-07-11 22:15:53 -0600

    Initial Commit

---
+++ TiledLayerDemo/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  TiledLayerDemo
+//
+//  Created by DJ Holt on 7/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return YES;
+}
+
+@end
