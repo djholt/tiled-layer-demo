9cdf2648cc726d2fbd604518609e0ccc95b62bbd
DJ Holt <dj@holt.dj>
2012-07-11 23:06:04 -0600

    Adjust size of TiledImageView

--- TiledLayerDemo/ViewController.m
+++ TiledLayerDemo/ViewController.m
@@ -24,6 +24,7 @@
 	// Do any additional setup after loading the view, typically from a nib.
 
     self.scrollView.delegate = self;
+    self.scrollView.contentSize = self.tiledImageView.bounds.size;
 }
 
 - (void)viewDidUnload
